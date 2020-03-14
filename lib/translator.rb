80ddda54a8c9d3a0b26d3e4cfec2bd7f52bca000
John Tom Jr <johntjr.22@gmail.com>
2020-03-14 05:26:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(yaml_file,emoticons)
   response = nil 
   library["get_emoticon"].each do |english, japanese|
     if emoticon == english 
-      response == japanese 
+      response = japanese 
     end
   end 
   
